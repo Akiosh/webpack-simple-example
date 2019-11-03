a02e8d269d7a78c2c668e259dc698966a41cfe03
aliaksandr_prakapovich <wherearemybigdonuts@gmail.com>
2019-11-03 17:20:10 +0300

    Asset Management: Loading Images.

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,34 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-      rules: [
-          {
-              test: /\.css$/,
-              use: [
-                  'style-loader',
-                  'css-loader'
-              ]
-          }
-      ]
-  }
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(svg|jpg|png|gif)/,
+                use: [
+                    {
+                        // https://www.npmjs.com/package/file-loader
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: './images',
+                            useRelativePath: true
+                        }
+                    },
+                ]
+            },
+        ]
+    }
 };
